5d7da28e864e2861f0e66f96281ad23e9adcc7ac
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2022-08-15 00:18:07 -0300

    (update) Python Basico - Ex_2 - Comments Added

--- PythonBasico/Ex_2.py
+++ PythonBasico/Ex_2.py
@@ -30,3 +30,25 @@ primos = []
 primos = crivo_de_eratostenes(n)
 print("\nO maior divisor primo desse número é:")
 print(maior_divisor_primo(n, primos))
+
+"""
+EXPLICAÇÃO:
+Para esse programa, busquei aumentar a eficiência no processamento
+e, para isso, fiz uma função que cria uma lista com os números primos
+com base no crivo de eratóstenes. Depois disso, utilizando a função
+maior_divisor_primo, ocorre a busca pelo maior divisor primo do número
+fornecido pelo usuário.
+
+Exemplos:
+    Entrada: 25
+    Saída: 5
+
+    Entrada: 100
+    Saída: 5
+
+    Entrada: 169
+    Saída: 13
+
+    Entrada: 91
+    Saída: 13
+"""
