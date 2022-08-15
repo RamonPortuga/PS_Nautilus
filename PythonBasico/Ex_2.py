3abb9594d3343bb75364da6a556df6c43f449fb2
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2022-08-14 23:05:58 -0300

    (add) Python Basico - Ex_2

---
+++ PythonBasico/Ex_2.py
@@ -0,0 +1,32 @@
+#2- Faça um programa que diga o maior divisor primo de um número dado como input
+def crivo_de_eratostenes(n):
+    numeros = [True] * (n + 1)
+    
+    numeros[0] = False
+    numeros[1] = False
+    
+    primos = []
+    
+    for numero, primo in enumerate(numeros):
+        if primo:
+            primos.append(numero)
+            
+            for i in range(numero * 2, n + 1, numero):
+                numeros[i] = False
+    return primos
+
+def maior_divisor_primo(n, primos):
+    maior = 0
+    for i in range (0, len(primos)):
+        if(primos[i] <= n):
+            if(n % primos[i] == 0):
+                maior = primos[i]
+        else:
+            break
+    return maior
+
+n = int(input("Entre com o número desejado:\t"))
+primos = []
+primos = crivo_de_eratostenes(n)
+print("\nO maior divisor primo desse número é:")
+print(maior_divisor_primo(n, primos))
