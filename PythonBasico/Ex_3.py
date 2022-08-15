7711047fc3fff806c0f6b21b1ed92f76ae492085
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2022-08-15 00:24:05 -0300

    (update) Python Basico - Ex_3 - Comments Added

--- PythonBasico/Ex_3.py
+++ PythonBasico/Ex_3.py
@@ -7,3 +7,27 @@ def checa_palindromo(n):
 
 n = input("Entre com o número desejado:\t")
 checa_palindromo(n)
+
+"""
+EXPLICAÇÃO:
+Nesse programa, bastou-se criar uma função checa_palindromo e, dentro
+dela, fazer uso de manipulação de strings para checar o número dado.
+Uma breve observação é que a entrada não foi lida como um número int
+ou float, para facilitar a manipulação.
+
+Exemplos:
+    Entrada: 999
+    Saída: O número digitado é um palíndromo
+
+    Entrada: 10
+    Saída: O número digitado não é um palíndromo
+
+    Entrada: 9
+    Saída: O número digitado é um palíndromo
+
+    Entrada: 858
+    Saída: O número digitado é um palíndromo
+
+    Entrada: 716
+    Saída: O número digitado não é um palíndromo
+"""
