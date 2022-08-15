424ebb6fb3aebada579647cc13d7ee38d18e673e
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2022-08-15 00:12:09 -0300

    (update) Python Basico - Ex_1 - Comments Added

--- PythonBasico/Ex_1.py
+++ PythonBasico/Ex_1.py
@@ -14,3 +14,21 @@ def checa_lista(lista):
 
 lista = [np.float64(x) for x in input("Entre com a lista = ").split(',')]
 checa_lista(lista)
+
+"""
+EXPLICAÇÃO:
+O funcionamento desse programa é bem simples, o programa lê a lista
+usando list comprehension e passa por parâmetro a lista lida para a
+função checa_lista(lista) e compara o primeiro elemento da lista com
+o último.
+
+Exemplos:
+    Entrada: 1,2,3,4,5,6,7,1
+    Saída: O primeiro item e o último são iguais
+
+    Entrada: 1,2,1,3,4,5
+    Saída: O primeiro item e o último são diferentes
+
+    Entrada: 1, 1.0
+    Saída: O primeiro item e o último são iguais
+"""
