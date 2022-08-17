0616a94bf3382ccd6633e60a0b1079484c8a454d
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2022-08-17 10:31:54 -0300

    (add) Python POO - Ex_1

---
+++ PythonPOO/Ex_1.py
@@ -0,0 +1,12 @@
+"""
+Implemente um modelo que descreva os AUVs da UFRJ Nautilus
+
+Requerimentos 
+
+Deve conter os atributos: número de thursters, lista com sensores, ano de construção, nome do veículo, e no mínimo, mais 1 atributo de livre escolha 
+Deve conter métodos para:
+Exibir todos os AUVs em tabela
+Exibir os robôs individulmente
+Rankear os robôs do mais novo para o mais antigo
+Deve conter outro método de livre escolha
+"""
